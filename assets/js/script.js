2f61d9beb8816f2ae6e7cd787d2b1494461fc463
SphinxGo <go.beltran.87@gmail.com>
2024-02-28 18:18:42 -0300

    integracion de navbar

---
+++ assets/js/script.js
@@ -0,0 +1,6 @@
+const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+
+$("#boton").click(function(){
+   alert("Su mensaje fue enviado satisfactoriamente...")
+});
